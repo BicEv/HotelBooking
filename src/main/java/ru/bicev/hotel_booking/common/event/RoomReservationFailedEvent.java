742b83947c04a3d128acdee8ac1045d7a61e7195
BicEv <bickbovev@gmail.com>
2025-08-06 17:45:48 +0300

    Removed unused classes

--- src/main/java/ru/bicev/hotel_booking/common/event/RoomReservationFailedEvent.java
+++
@@ -1,9 +0,0 @@
-package ru.bicev.hotel_booking.common.event;
-
-import java.time.Instant;
-import java.util.UUID;
-
-public record RoomReservationFailedEvent(UUID eventId, Instant timestamp, UUID bookingId, UUID roomId, String reason)
-                implements Event {
-
-}
--- src/main/java/ru/bicev/hotel_booking/common/event/RoomReservedEvent.java
+++
@@ -1,8 +0,0 @@
-package ru.bicev.hotel_booking.common.event;
-
-import java.time.Instant;
-import java.util.UUID;
-
-public record RoomReservedEvent(UUID eventId, Instant timestamp, UUID bookingId, UUID roomId) implements Event {
-
-}
